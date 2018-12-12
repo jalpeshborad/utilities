d320ae28fa0a4147c77393cf2d01d20f793df597
Jalpesh Borad <jalpeshborad@gmail.com>
2018-12-12 14:48:05 +0530

    Added mail and logger methods

---
+++ setup.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# -*- coding: utf-8 -*-
+
+__author__ = "Jalpesh Borad"
+__copyright__ = "Copyright 2018"
+
+__version__ = "0.0.1"
+__maintainer__ = "Jalpesh Borad"
+__email__ = "jalpeshborad@gmail.com"
+__status__ = "Development"
+
+import setuptools
+
+
+setuptools.setup(
+    name="general_utilities",
+    version="0.1",
+    author="Jalpesh Borad",
+    author_email="jalpeshborad@gmail.com",
+    description="A general utilities for day-to-day python programs",
+
+)
